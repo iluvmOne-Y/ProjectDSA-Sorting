700aebba1c56d8c8987028d8fe9b5db2d16fda68
Livens <dangkhoah77@gmail.com>
2025-03-16 12:17:17 +0700

    [ref] Refactor algorithm structure and update function signatures for sorting algorithms

--- source/algorithms.hpp
+++ source/algorithms.hpp
@@ -5,31 +5,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
+
 typedef long long (*SortFunction)(int[], int);
 
 struct AlgorithmInfo {
-  string name;
-  SortFunction sort;
+    string name;
+    SortFunction sort;
 };
+
 extern std::vector<AlgorithmInfo> algorithms;
-inline int findMax(int a[], int n) {
-  int max = a[0];
-  for (int i = 1; i < n; i++) {
-    if (a[i] > max) {
-      max = a[i];
-    }
-  }
-  return max;
-}
 
 long long SelectionSort(int a[], int n);
 long long InsertionSort(int a[], int n);
 long long QuickSort(int a[], int n);
-
 long long RadixSort(int a[], int n);
-long long shakerSort(int data[], int size, long long &count);
-long long shellSort(int data[], int size, long long &count);
+long long ShakerSort(int data[], int n);
+long long ShellSort(int data[], int n);
 long long HeapSort(int a[], int n);
 long long BubbleSort(int a[], int n);
+long long BinaryInsertionSort(int a[], int n);
+long long MergeSort(int a[], int n);
+long long CountingSort(int a[], int n);
+long long FlashSort(int a[], int n);
+
 #endif
