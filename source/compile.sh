27945e36431d0a722090ca8c46ac1a613ff2de16
Livens <dangkhoah77@gmail.com>
2025-03-16 13:28:27 +0700

    [fix] Update compile script to output executable as main.exe

--- source/compile.sh
+++ source/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile the main.cpp file
-g++ -o main main.cpp algorithms.cpp Command.cpp DataGenerator.cpp HelperFunction.cpp
+g++ -o main.exe main.cpp algorithms.cpp Command.cpp DataGenerator.cpp HelperFunction.cpp
 
 # Check if the compilation was successful
 if [ $? -eq 0 ]; then
